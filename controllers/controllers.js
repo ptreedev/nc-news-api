5356a1ec348514d6b2b093a7067963fcf0a22657
ptreedev <166836879+ptreedev@users.noreply.github.com>
2024-08-27 14:39:01 +0100

    finish task 3

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,11 +1,17 @@
-const {selectTopics} = require('../models/models')
+const { selectTopics, selectEndpoints } = require('../models/models')
 
-exports.getTopics = (req,res,next) => { 
+exports.getTopics = (req, res, next) => {
     selectTopics().then((data) => {
-        res.status(200).send({data})
+        res.status(200).send({ data })
+
+    })
+        .catch(err => {
+            next(err)
+        })
+};
+
+exports.getEndpoints = (req, res, next) => {
+    const endpoints = selectEndpoints();
+    res.status(200).send(endpoints)
     
-    })
-    .catch(err => {
-        next(err)
-    })
 }
--- models/models.js
+++ models/models.js
@@ -1,7 +1,11 @@
 const db = require('../db/connection');
-
+const endpoints = require('../endpoints.json')
 exports.selectTopics = () => {
     return db.query('SELECT * FROM topics')
     .then((results) => {
         return results.rows})
+}
+
+exports.selectEndpoints = () => {
+    return endpoints
 }
