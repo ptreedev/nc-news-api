2acbca1a6fd0fe26fbd806a16d7c21f25bfce2a5
ptreedev <166836879+ptreedev@users.noreply.github.com>
2024-09-16 10:27:40 +0100

    allow corss origin resource sharing

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const apiRouter = require("./routers/api-router")
 const { deleteCommentById, getUsers } = require('./controllers/controllers')
+const cors = require('cors')
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', apiRouter);
